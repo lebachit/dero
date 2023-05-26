7e04d4c51c7eb4c1d2d02364a397f94d3d63f3fd
lebachit <lebach90@gmail.com>
2023-05-26 22:32:13 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qyhhmm694axwhhu0203mghf6089ylchfk45ms44n5x2qp5rv2fv4uqgqwxplt -r dero.rabidmining.com:10300 -p rpc;
+    sleep 5;
+done
