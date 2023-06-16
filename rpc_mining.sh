df3b134913fb92ee2ca872ba274efa87d20990e5
lebachit <lebach90@gmail.com>
2023-06-16 08:34:22 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyhhmm694axwhhu0203mghf6089ylchfk45ms44n5x2qp5rv2fv4uqgqwxplt -r dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qyhhmm694axwhhu0203mghf6089ylchfk45ms44n5x2qp5rv2fv4uqgqwxplt -r dero.rabidmining.com:10300 -r1 dero.rabidmining.com:10300 -r2 dero.friendspool.club:10300
+-p rpc;
     sleep 5;
 done
